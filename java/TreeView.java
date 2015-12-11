8884ee31b8dc348dbf2bcd28c00e29e6de1f0450
huaixiaoz <hello@itmp.top>
2015-12-11 14:54:19 +0800

    update

--- java/TreeView.java
+++ java/TreeView.java
@@ -2,6 +2,11 @@ import java.io.File;
 
 
 public class TreeView{
+    static int levels = 0;
+    static int fls = 0;
+    static int dirs = 0 - 1;
+    static boolean last = false;
+
     public static void main(String[] args){
         if(args.length != 1){
             System.out.println("Usage: TreeView [Dir] " + args.length + args[0]);
@@ -15,27 +20,43 @@ public class TreeView{
             return;
         }
 
-        System.out.println("- " + file.getName());
-        index_file(file);
+        System.out.println(file.getName());
+        index_file(file, levels);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.print(" Files: " + fls + "\t");
+        System.out.print(" Dirs: " + dirs + "\t");
+        System.out.println(" Levels: " + levels);
 
     }
 
 
-    public static int index_file(File file){
-        int rtn = 0;
+    public static int index_file(File file, int level){
+        //System.out.print("\t");
+        for(int i = 1; i < level; i++)
+        System.out.print("│  ");
+        //System.out.println("├──" + file.getName());
         if(file.isFile()){
-            System.out.println("  |- " + file.getName());
-            return rtn;
+            fls++;
+            System.out.println("└──" + file.getName());
+            return level;
         }
         if(file.isDirectory()){
+            dirs++;
             File[] files = file.listFiles();
-            int i = 0;
-            for (; i < files.length; i++){
-                index_file(files[i]);
+            if(level != 0){
+                System.out.println("├──" + file.getName());
             }
-            return i;
+            if(files.length > 0 && level >= levels){
+                levels = level + 1;
+            }
+
+            for (int i = 0; i < files.length; i++){
+                index_file(files[i], level + 1);
+            }
         }
-        return -1;
+    return level;
     }
 }
 
