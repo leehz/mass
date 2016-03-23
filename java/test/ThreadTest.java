8dbfde120c0e00d65ed748c47a237674d8fc6f08
huaixiaoz <hello@itmp.top>
2016-03-23 09:24:47 +0000

    synchronized Thread

---
+++ java/test/ThreadTest.java
@@ -0,0 +1,13 @@
+public class ThreadTest {  
+    public static void main(String[] args) {  
+        Runnable runnable = new Runnable() {  
+            Count count = new Count();  
+            public void run() {  
+                count.count();  
+            }  
+        };  
+        for(int i = 0; i < 10; i++) {  
+            new Thread(runnable).start();  
+        }  
+    }  
+}  
