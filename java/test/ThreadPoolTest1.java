35ec8152dc89753e8fa52ec3d7c421407ad477cc
leehz <hello@itmp.top>
2016-03-25 10:01:13 +0800

    Threads

---
+++ java/test/ThreadPoolTest1.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+public class ThreadPoolTest1 {
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        for(int i = 1; i < 5; i++) {
+            final int taskID = i;
+            threadPool.execute(new Runnable() {
+                public void run() {
+                    for(int i = 1; i < 5; i++) {
+                        try {
+                            Thread.sleep(100);// 为了测试出效果，让每次任务执行都需要一定时间
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("第" + taskID + "次任务的第" + i + "次执行");
+                    }
+                }
+            });
+        }
+        threadPool.shutdown();// 任务执行完毕，关闭线程池
+    }
+}
